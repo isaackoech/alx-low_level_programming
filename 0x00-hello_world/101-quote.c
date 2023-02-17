db60c84114a3e5c9ce9b59602b5a6aab12c3f8c9
isaackoech <isaackoech96@gmail.com>
2023-02-17 11:14:31 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * This code print a string
+ * Return: 1 if success
+ */
+
+int main(void)
+{	
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return(1);
+
+}
