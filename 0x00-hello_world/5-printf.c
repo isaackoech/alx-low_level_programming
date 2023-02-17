147fb48abed201bd9d0432422afb90481450c855
isaackoech <isaackoech96@gmail.com>
2023-02-17 07:33:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * A function that prints exactly "with proper grammar, but the outcome is a piece of art"
+ * Returns 0 if success
+ */
+
+int main(void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art");
+	Return(0)
+}
