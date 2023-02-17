b7ff013dd99d1177c38f5c7a78ffec4a94d341f4
isaackoech <isaackoech96@gmail.com>
2023-02-17 07:54:16 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * A function that prints a string
- * Returns 0 if success
- */
 
+/**
+* Description: main - Prints out a string to stdout.
+*
+* Return: 0 if success.
+*/
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
