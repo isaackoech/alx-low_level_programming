c1b6630bd4682d4e381ae66b3835d66b4338912f
isaackoech <isaackoech96@gmail.com>
2023-02-17 11:29:46 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * This code print a string
- * Return: 0 if success
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
-{	
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
-	return(1);
-
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
 }
