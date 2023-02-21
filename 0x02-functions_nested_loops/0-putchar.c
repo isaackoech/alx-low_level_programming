2012dccd4ea6689127e1be8ed9e29cbabe6d23e5
isaackoech <isaackoech96@gmail.com>
2023-02-21 00:27:07 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - a program that prints a string
+ * Return: 0 always (success)
+ */
+int main(void)
+{	printf("_putchar\n");
+	return (0);
+}
